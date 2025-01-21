b4fd49c7cf68fd6d66e4bd9252fb6798ee293aba
kidus <kidusefrem2@gmail.com>
2025-01-22 02:29:32 +0300

    Add wrong letter handling in JavaScript

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const wordE1 = document.getElementById('word');
 const words = ['application', 'programming', 'interface', 'wizard'];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 const correctLetters = [];
+const wrongLetters = [];
+const wrongLettersE1 = document.getElementById('wrong-letters');
+
+function updateWrongLetters() {
+    wrongLettersE1.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`).join('')}
+    `;
+}
 
 function displayWord() {
     wordE1.innerHTML = selectedWord
